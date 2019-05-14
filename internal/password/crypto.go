70c7d40425c6882f88d92f7090e414921ee23a90
Dino Omanovic <angor.mail@googlemail.com>
2019-05-14 21:16:50 +0200

    Make cryptosource private as only the password package should use it

--- internal/password/crypto.go
+++ internal/password/crypto.go
@@ -8,16 +8,16 @@ import (
 
 // CryptoSource is a custom implementation of a math/rand Source so that we can use the
 // math/rand package for secure random operations
-type CryptoSource struct{}
+type cryptoSource struct{}
 
 // Default instance can be used as concurrent access is allowed.
-var random = mathrand.New(CryptoSource{})
+var random = mathrand.New(cryptoSource{})
 
 // We do not need a seed as we read from crypto/rand.Reader
-func (c CryptoSource) Seed(seed int64) {}
+func (c cryptoSource) Seed(seed int64) {}
 
 // Int63 returns a cryptographically secure random int64
-func (c CryptoSource) Int63() int64 {
+func (c cryptoSource) Int63() int64 {
 	// Represent the int64 in bytes
 	var b [8]byte
 	// Read secure random bytes into a slice of our array
--- internal/password/crypto_test.go
+++ internal/password/crypto_test.go
@@ -6,7 +6,7 @@ import (
 
 func BenchmarkCryptoSource_Int63(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		c := CryptoSource{}
+		c := cryptoSource{}
 				c.Int63()
 	}
 }
