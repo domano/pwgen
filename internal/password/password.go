a4f1a5dd7f34a2f1c95dec537784387e24f937c5
Dino Omanovic <angor.mail@googlemail.com>
2019-05-18 19:40:51 +0200

    WIP swapper

--- internal/password/password.go
+++ internal/password/password.go
@@ -1,10 +1,16 @@
 // Package password provides types and functions for password generation.
 package password
 
+import (
+	"bytes"
+	"math"
+)
+
 // Generator can generate passwords with a given configuration
 // passed via functional Options in its constructor.
 type Generator struct {
 	minLength, specialChars, nums int
+	swap bool
 }
 
 // Option is the functional option type to allow variadic and
@@ -15,6 +21,8 @@ type Option func(*Generator)
 const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const numbers = "0123456789"
 const specialChars = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+const vowels, vowelNums = "aAeEiIoO", "4310"
+
 
 // NewGenerator will create a Generator which can generate passwords.
 // A number of Options can be passed to configure the resulting Generator.
@@ -47,25 +55,55 @@ func Numbers(amount int) Option {
 	}
 }
 
+// Swap configures if vowels should be swapped with numbers
+func Swap(shouldSwap bool) Option {
+	return func(g *Generator) {
+		g.swap = shouldSwap
+	}
+}
+
 // Password generates a password with the generators' configuration
 func (g Generator) Password() string {
 	var passwordBytes []byte
 
 	// Create numbers, special chars and letters for the password randomly
-	passwordBytes = append(passwordBytes, randomBytes(numbers, g.nums)...)
-	passwordBytes = append(passwordBytes, randomBytes(specialChars, g.specialChars)...)
-	if g.minLength > len(passwordBytes) {
-		passwordBytes = append(passwordBytes, randomBytes(letters, g.minLength-len(passwordBytes))...)
-	}
+	passwordBytes = g.generate(passwordBytes)
 
 	// Shuffle the password
-	var password = make([]byte, len(passwordBytes))
-	for i, v := range random.Perm(len(passwordBytes)) {
-		password[i] = passwordBytes[v]
-	}
+	password := g.shuffle(passwordBytes)
 
 	return string(password)
 }
+
+func (g Generator) generate(pw []byte) []byte {
+	pw = append(pw, randomBytes(numbers, g.nums)...)
+	pw = append(pw, randomBytes(specialChars, g.specialChars)...)
+	if g.minLength > len(pw) {
+		pw = append(pw, randomBytes(letters, g.minLength-len(pw))...)
+	}
+	return pw
+}
+
+func (g Generator) shuffle(passwordBytes []byte) []byte {
+	var password = make([]byte, len(passwordBytes))
+	for i, v := range random.Perm(len(passwordBytes)) {
+		if g.swap {
+			g.swapVowel(passwordBytes[v])
+		}
+		password[i] = passwordBytes[v]
+	}
+	return password
+}
+
+func (g Generator) swapVowel(char byte) byte{
+		index := bytes.IndexByte([]byte(vowels), char)
+		if index > 0 && random.Intn(2) == 1 {
+			vowelNumIndex := int(math.Ceil(float64(index) / 2.0))
+			return vowelNums[vowelNumIndex]
+		}
+		return char
+}
+
 
 func randomBytes(from string, length int) []byte {
 	str := make([]byte, length)
